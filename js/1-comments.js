a5da8d40b13d51e72a13c836a2c99bc8e0b64a14
unknown <maciekslotwinski@gmail.com>
2017-09-09 09:35:15 +0200

    komentarze

---
+++ js/1-comments.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// komentarz liniowy
+//Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga rerum non fugiat modi quam ad quo. Possimus recusandae dignissimos, deserunt repellendus delectus deleniti facere unde et laborum, maxime, animi maiores.
+
+/*Komentarz blokowy*/
+/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga rerum non fugiat modi quam ad quo. Possimus recusandae dignissimos, deserunt repellendus delectus deleniti facere unde et laborum, maxime, animi maiores.*/
