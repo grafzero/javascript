7933493cae195b0ba24f827216bd7a829eedabc6
unknown <maciekslotwinski@gmail.com>
2017-09-09 09:59:23 +0200

    variables

---
+++ js/2-variables.js
@@ -0,0 +1,16 @@
+'use strict';
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+console.log(kolor);
+
+imie = imieDamskie;
+
+console.log(imie);
+
